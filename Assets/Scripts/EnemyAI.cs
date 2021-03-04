c8fc1b4f9080d09d4ad54978ff2bbdf71ad95130
Joshua McCown <jpm623@msstate.edu>
2021-03-03 22:34:50 -0600

    added a spawn controller and a simple Ai controller to have skeletons spawn in specified locations and run at the current position of the player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent navMesh;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.SetDestination(target.position);
+    }
+}
